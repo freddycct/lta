a9a19e5ffb626a1aa2814a98939560c9d50cd433
freddy <freddy@MU3.(none)>
2014-03-10 13:33:12 +0800

    modified gnu parallel script

--- julia/extract_svc_num.sh
+++ julia/extract_svc_num.sh
@@ -2,4 +2,4 @@
 
 max_jobs=6
 
-parallel --no-notice --max-procs ${max_jobs} "julia extract_svc_num.jl {1} > {1}.txt" :::: ../data/20111101_20111106/all_service_numbers_data_20111101_20111106
+parallel --no-notice --max-procs ${max_jobs} "zcat ../data/ride_data_20111101_20111106.gz | julia extract_svc_num.jl {1} > ../data/20111101_20111106/{1}.txt" :::: ../data/20111101_20111106/all_service_numbers_data_20111101_20111106
