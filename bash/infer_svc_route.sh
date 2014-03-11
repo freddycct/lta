8949d59157f0dac1cdca898afb967e200baea768
freddy <freddy@MU3.(none)>
2014-03-11 13:55:15 +0800

    updated script

--- bash/infer_svc_route.sh
+++ bash/infer_svc_route.sh
@@ -2,4 +2,4 @@
 
 max_jobs=6
 
-parallel --no-notice --max-procs ${max_jobs} "cat ../data/20111101_20111106/{1}.txt | julia infer_svc_route.jl > ../data/20111101_20111106/log/{1}.log" :::: ../data/20111101_20111106/all_service_numbers_data_20111101_20111106
+parallel --no-notice --max-procs ${max_jobs} "cat ../data/20111101/{1}.txt | julia infer_svc_route.jl > ../data/20111101/log/{1}.log" :::: ../data/20111101/all_service_numbers_data_20111101
