3135dc9a483cc7c37a26ed498b2e6e9af33a8f7d
Freddy Chua <freddycct@gmail.com>
2014-03-13 13:49:05 +0800

    added comments, deleted redundant script

--- bash/infer_day_svc_route.sh
+++ bash/infer_day_svc_route.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#plot the routes of each bus service for everyday
+
 max_job=6
 
 #parallel --no-notice --max-procs ${max_job} 'cat ../data/{1}/bus_records/{2}.txt" :::: ../data/file_list
