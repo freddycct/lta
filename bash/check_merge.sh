eae27d378066810f4aee5db24092d0e21de3c7d0
Freddy Chua <freddycct@gmail.com>
2014-03-16 14:24:13 +0800

    updated scripts and programs for checking number of merges

---
+++ bash/check_merge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+max_jobs=6
+
+parallel --no-notice --max-procs ${max_jobs} "ls -l ../data/{1}/logs/ | julia ../julia/check_merge.jl | sort -n > ../data/{1}/success" :::: ../data/file_list
+
--- julia/check_merge.jl
+++ julia/check_merge.jl
@@ -1,4 +1,15 @@
+readline(STDIN)
+
 while !eof(STDIN)
 	line = readline(STDIN)
 	
+	fields = split(line)
+
+	size = parseint(fields[5])
+	svc_num = fields[9]
+
+	if size <= 0
+		println(svc_num)
+	end
 end
+
