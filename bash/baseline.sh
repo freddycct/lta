60d5a77d43ea9aef83ebce2fe0cf944ae381a50e
Freddy Chua <freddycct@gmail.com>
2014-03-27 16:17:29 +0800

    update

--- bash/baseline.sh
+++ bash/baseline.sh
@@ -2,4 +2,5 @@
 
 max_jobs=6
 
-parallel --no-notice --max-procs ${max_jobs} "julia ../julia/baseline.jl ../data {1} > ../data/{1}/baseline_params.txt" :::: ../data/file_list
+#parallel --no-notice --max-procs ${max_jobs} "julia ../julia/baseline.jl ../data {1} > ../data/{1}/baseline_params.txt" :::: ../data/file_list
+parallel --no-notice --max-procs ${max_jobs} "julia ../julia/baseline2.jl ../data {1} > ../data/{1}/baseline2_params.txt" :::: ../data/file_list
