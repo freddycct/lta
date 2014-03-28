7fc4f42bafb999c0312910e635229231c79dc249
Freddy Chua <freddycct@gmail.com>
2014-03-28 14:32:30 +0800

    update print looping service

--- julia/infer_svc_route.jl
+++ julia/infer_svc_route.jl
@@ -120,10 +120,13 @@ for dict_pair1 in bus_services
 		@printf(fid, "Direction_%d: %s:%0.2f", direction, get_id(node), node.distance_to_next)
 		num_stops = 1
 		current_node = node.next
-		while (current_node != node) && 
-			(current_node != bus_service.routes[direction].head) && (num_stops < 1000)
+		while (current_node != node) && (num_stops < 1000)
 			@printf(fid, " %s:%0.2f", get_id(current_node), current_node.distance_to_next)
-			num_stops = num_stops + 1
+			if current_node == bus_service.routes[direction].head
+				println("Loop Detected")
+				break
+			end
+			num_stops += 1
 			node = current_node
 			current_node = current_node.next
 		end
