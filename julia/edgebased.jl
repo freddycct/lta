04428667d99e367b1fb725e213e272e8652a43c4
Freddy Chua <freddycct@gmail.com>
2014-04-01 20:39:37 +0800

    update to read all

--- julia/edgebased.jl
+++ julia/edgebased.jl
@@ -181,8 +181,8 @@ function read_all_records(prefix::ASCIIString, date::ASCIIString,
 
     records = Array(Record, 0)
     fid_success = open(@sprintf("%s/%s/success", prefix, date), "r")
-    #while !eof(fid_success)
-    for i=1:20
+    while !eof(fid_success)
+    #for i=1:20
         line = readline(fid_success)
         bus_no = strip(line)
         #bus_no = "7"
