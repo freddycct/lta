642ebc74b50efd8a6221f72dff0a9eeb3c15c020
Freddy Chua <freddycct@gmail.com>
2014-04-12 15:56:50 +0800

    ...

--- bash/extract_day_records.sh
+++ bash/extract_day_records.sh
@@ -4,4 +4,4 @@
 
 max_jobs=6
 
-parallel --no-notice --max-procs ${max_jobs} "zcat ../data/raw_gz/{1}.gz | julia ../julia/extract_day_records.jl" :::: ../data/file_list
+parallel --no-notice --max-procs ${max_jobs} "zcat ../data/raw_gz/{1}.gz | julia ../julia/extract_day_records.jl" :::: ../data/raw_file_list
