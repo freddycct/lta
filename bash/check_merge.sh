6407daaa00931ea98e168b38ae594c6eb0b06130
Freddy Chua <freddycct@gmail.com>
2014-04-24 11:32:46 +0800

    ...

--- bash/check_merge.sh
+++ bash/check_merge.sh
@@ -2,5 +2,5 @@
 
 max_jobs=6
 
-parallel --no-notice --max-procs ${max_jobs} "ls -l ../data/{1}/logs/ | julia ../julia/check_merge.jl | sort -n > ../data/{1}/success" :::: ../data/file_list
+parallel --no-notice --max-procs ${max_jobs} "ls -l ../data/{1}/logs/ | julia ../julia/check_merge.jl | sort -n > ../data/{1}/success" :::: ../data/file_list.txt
 
